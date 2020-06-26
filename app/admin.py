c89b9f219acd4a06a7c310776a9845974177ea48
Mew Forest <mew.forest@gmail.com>
2020-06-26 21:10:29 +0300

    добавил авторизацию для админки

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,20 @@ from app.models import User, Post
 from app import app, db
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
+from flask_login import current_user
+from flask import redirect, url_for
+
+
+class ModelViewSecured(ModelView):
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            return current_user.email == 'moscow1@itstep.org'
+        return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
 
 
 admin = Admin(app, name='blog-admin', template_mode='bootstrap3')
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelViewSecured(User, db.session))
+admin.add_view(ModelViewSecured(Post, db.session))
